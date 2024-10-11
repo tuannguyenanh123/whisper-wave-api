fb0a82a7909cb4d09aa591571c65f1681c19fac2
tuananh <tuanan.devcandy@gmail.com>
2024-10-11 07:17:27 +0700

    fix port for front end

--- src/main.ts
+++ src/main.ts
@@ -5,18 +5,18 @@ import * as graphqlUploadExpress from 'graphql-upload/graphqlUploadExpress.js';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:4200', 'http://localhost:8080'], // replace with your frontend URLs
+    origin: ['http://localhost:5173', 'http://localhost:8080'], // replace with your frontend URLs
     allowedHeaders: [
       'Accept',
       'Authorization',
       'Origin',
       'Content-Type',
       'X-Requested-With',
-      "apollo-require-preflight"
+      'apollo-require-preflight',
     ],
     credentials: true,
-  })
-  app.use(graphqlUploadExpress({maxFileSize: 1000000, maxFiles: 1 })); // use graphql-upload middleware
+  });
+  app.use(graphqlUploadExpress({ maxFileSize: 1000000, maxFiles: 1 })); // use graphql-upload middleware
   await app.listen(3000);
 }
 bootstrap();
